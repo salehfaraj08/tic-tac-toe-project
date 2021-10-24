6faed73c56a8c62debfe744138caa6cad8e87caa
salehfaraj08 <89396137+salehfaraj08@users.noreply.github.com>
2021-10-24 11:08:50 +0300

    Update game.jsx

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -39,7 +39,7 @@ const Game = () => {
         const current = historyPoint[stepNumber];
         let shapes=[];
         shapes.push(...current);
-        
+        console.log(historyPoint);
         if (winner || shapes[i]) return;
         shapes[i] = xOro;
         setHistoryBoardState([...historyPoint, shapes]);
